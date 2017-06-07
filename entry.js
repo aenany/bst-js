f19785efd15902aaac4ffad667672bdbb6d256c9
A Enany <aenany@users.noreply.github.com>
2017-06-06 23:30:59 -0400

    entry.js cleanup

--- entry.js
+++ entry.js
@@ -1,14 +1,13 @@
 var counter, randomValue, constructedNode;
 var theTree = new Tree();
 
-for (counter = 0; counter < 10; counter++){
-    randomValue = Math.floor(Math.random()* 100);    
-    theTree.addValue(randomValue);    
+for (counter = 0; counter < 10; counter++) {
+    randomValue = Math.floor(Math.random() * 100);
+    theTree.addValue(randomValue);
 }
 
 console.log(theTree);
 
 theTree.traverse();
-
 
 console.info('Binary Search Tree: theTree');
